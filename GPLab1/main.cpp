811024d7ea0f9a72ee84c81eac18b1e3fa5a43b5
ckinne1771 <ckinney1771@gmail.com>
2014-09-24 16:34:38 +0100

    Completed 4/6 exercises.
    Exercises 5 and 6 still to be completed.

--- GPLab1/main.cpp
+++ GPLab1/main.cpp
@@ -116,9 +116,20 @@ void render()
 
 	glBegin(GL_TRIANGLES);
 		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(-1.0f, 1.0f, 0.0f);
+		glColor3f(0.0f, 1.0f, 0.0f);
 		glVertex3f(-1.0f, -1.0f, 0.0f);
+		glColor3f(0.0f, 0.0f, 1.0f);
 		glVertex3f(1.0f, -1.0f, 0.0f);
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	glColor3f(4.0f, 0.0f, 0.0f);
+	glVertex3f(1.5f, 1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(2.0f, 1.0f, 0.0f);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(2.0f, 0.0f, 0.0f);
 	glEnd();
 	
 
